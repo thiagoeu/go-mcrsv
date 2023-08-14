38d0453ada8cc85851caa2bb146796af2105df73
thiagoeu <araujo.thiago1051@gmail.com>
2023-08-14 09:23:47 -0300

    first commit

---
+++ internal/entities/category.go
@@ -0,0 +1,10 @@
+package entities
+
+import "time"
+
+type Category struct {
+	ID        uint      `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
